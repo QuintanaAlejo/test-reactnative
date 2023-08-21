57300fca5b1b01c1ef74482f2918f59d1e7d7577
Nicolas <nicoferni55@gmail.com>
2023-08-20 23:51:56 -0300

    first commit nicolas

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>ASHEEEEE</Text>
+      <Text>LANDING REPARAPP
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FF0000',
     alignItems: 'center',
     justifyContent: 'center',
   },
